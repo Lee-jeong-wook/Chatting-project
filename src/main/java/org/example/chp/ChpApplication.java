f2468bd0c966cc024b4789f643c4a52db8cc573a
Lee-jeong-wook <b01024134020@gmail.com>
2024-03-19 18:50:11 +0900

    CHP 프로젝트 시작

---
+++ src/main/java/org/example/chp/ChpApplication.java
@@ -0,0 +1,13 @@
+package org.example.chp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chp/ChpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
